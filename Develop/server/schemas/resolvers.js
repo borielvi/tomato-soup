f43eec3d2a9e735c65e05b8a79e1861efe8cacc9
borielvi <borielvi@isu.edu>
2020-10-18 14:08:06 -0700

    "added dependencies"

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,1 +1,20 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.User) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+                
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+
+    }
+}
